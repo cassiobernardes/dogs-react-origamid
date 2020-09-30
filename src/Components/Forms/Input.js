6b60724808a1556aa36a7b4de4693257e85f60a7
Cássio Bernardes <cassio.bernardes@compasso.com.br>
2020-09-29 23:20:51 -0300

    validate token

--- src/Components/Forms/Input.js
+++ src/Components/Forms/Input.js
@@ -7,7 +7,15 @@ const Input = ({ label, type, name, value, onChange, error, onBlur }) => {
       <label htmlFor={name} className={styles.label}>
         {label}
       </label>
-      <input id={name} name={name} className={styles.input} type={type} value={value} onChange={onChange} onBlur={onBlur} />
+      <input
+        id={name}
+        name={name}
+        className={styles.input}
+        type={type}
+        value={value}
+        onChange={onChange}
+        onBlur={onBlur}
+      />
       {error && <p className={styles.error}>{error}</p>}
     </div>
   );
