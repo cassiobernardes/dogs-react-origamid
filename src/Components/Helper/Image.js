61a3d9e87d48d96e6a43a05246eb9e5b6e9cfe7c
Cássio Bernardes <cassio.bernardes@compasso.com.br>
2020-10-06 21:34:51 -0300

    image skeleton

---
+++ src/Components/Helper/Image.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import styles from './Image.module.css';
+
+const Image = ({ alt, src, ...props }) => {
+  const [skeleton, setSkeleton] = React.useState(true);
+
+  function handleLoad({ target }) {
+    target.style.opacity = 1;
+    setSkeleton(false);
+  }
+  return (
+    <div className={styles.wrapper}>
+      {skeleton && <div className={styles.skeleton}></div>}
+
+      <img
+        onLoad={handleLoad}
+        className={styles.img}
+        src={src}
+        alt={alt}
+        {...props}
+      />
+    </div>
+  );
+};
+
+export default Image;
